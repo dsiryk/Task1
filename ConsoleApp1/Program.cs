1299e46b15502c60a67eed669c90caeb004f0ef4
deboo <deboo@HUSKER>
2025-02-05 21:54:57 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((a+b+c) / 3);
